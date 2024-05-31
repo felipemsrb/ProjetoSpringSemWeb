6e84e4770b6c53397c459b398a8f867ccef9aa58
Felipe Mota <felipe.msrb@gmail.com>
2024-05-31 11:33:00 +0100

    refatorando o código para informar mais dados da série

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -18,17 +18,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
         Principal principal = new Principal();
         principal.exibeMenu();
 
-//		List<DadosTemporada> temporadas = new ArrayList<>();
-//
-//		//usando um bloco de repetição para adicionar o total de temporada numa lista
-//		for (int i = 1; i <= dados.totalDeTemporadas(); i++){
-//			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=lost&season=" + i +"&apikey=2c3fc37&");
-//			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-//			temporadas.add(dadosTemporada);
-//
-//		}
-//
-//		temporadas.forEach(System.out::println);
+
 
 
 
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,12 @@
 package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 //criando constantes e menu interativo para que o código fique mais limpo
 public class Principal {
@@ -19,6 +22,17 @@ public class Principal {
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        //usando um bloco de repetição para adicionar o total de temporada numa lista
+        for (int i = 1; i <= dados.totalDeTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + "&apikey=2c3fc37&");
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+        }
+
+        temporadas.forEach(System.out::println);
 
     }
 }
